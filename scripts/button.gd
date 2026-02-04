591a289b27e357388fd107bdcc5817c39ff6e208
Lyle Watkins <85589327+llwatkin@users.noreply.github.com>
2026-02-03 23:41:37 -0800

    Added animations to buttons

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,5 +1,7 @@
 extends StaticBody3D
 
+@onready var _anim_player: AnimationPlayer = $AnimationPlayer
+
 var _mouse_over: bool = false
 var _mouse_down: bool = false
 var _time_down: float = 0
@@ -27,14 +29,14 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	_mouse_over = false
 	if _mouse_down:
-		#$AnimationPlayer.play("button_animations/button_release")
+		_anim_player.play_backwards("button_press")
 		_mouse_down = false
 
 
 func _input(event: InputEvent) -> void:
 	if _mouse_over and event.is_action_pressed("interact"):
-		#$AnimationPlayer.play("button_animations/button_press")
+		_anim_player.play("button_press")
 		_mouse_down = true
 	if _mouse_over and event.is_action_released("interact"):
-		#$AnimationPlayer.play("button_animations/button_release")
+		_anim_player.play_backwards("button_press")
 		_mouse_down = false
