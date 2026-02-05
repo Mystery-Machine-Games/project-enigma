bb64087545cd73d3c3423424a4a1b568d90afd64
Lyle Watkins <85589327+llwatkin@users.noreply.github.com>
2026-02-04 19:42:35 -0800

    Randomized button positions and colors

--- scripts/button_module.gd
+++ scripts/button_module.gd
@@ -1,5 +1,8 @@
 extends Node
 
+@export var button_positions: Array[Vector3]
+@export var button_colors: Array[Material]
+
 enum Interaction {PRESS, HOLD}
 
 const NUM_BUTTONS: int = 3
@@ -7,7 +10,7 @@ const NUM_BUTTONS: int = 3
 const POSSIBLE_INTERACTION_NUMS: Array[int] = [1, 2, 3, 4, 5]
 
 var _interaction: Interaction = Interaction.values().pick_random()
-@onready var _order: Array[Node] = get_children()
+@onready var _buttons: Array[Node] = get_children()
 var _interaction_nums: Array[int] = POSSIBLE_INTERACTION_NUMS.duplicate()
 var _interaction_history: Dictionary[Node, float]
 
@@ -15,15 +18,18 @@ signal buttons_correct
 
 
 func _ready() -> void:
-	_order.shuffle()
+	_buttons.shuffle()
 	for i in range(POSSIBLE_INTERACTION_NUMS.size() - NUM_BUTTONS):
 		var random_num: int = _interaction_nums.pick_random()
 		var random_num_index: int = _interaction_nums.find(random_num)
 		_interaction_nums.remove_at(random_num_index)
-	print("[BUTTON_MODULE][SOLUTION] Interaction: ", Interaction.keys()[_interaction], " Order: ", _order, " Numbers: ", _interaction_nums)
+	print("[BUTTON_MODULE][SOLUTION] Interaction: ", Interaction.keys()[_interaction], " Order: ", _buttons, " Numbers: ", _interaction_nums)
 	
-	for button in _order:
+	for i in range(_buttons.size()):
+		var button: StaticBody3D = _buttons[i]
 		button.button_pressed.connect(_log_interaction)
+		button.position = button_positions[i]
+		button.find_child("Button").material_override = button_colors.pick_random()
 
 
 func _log_interaction(button: Node, time_down: float) -> void:
@@ -44,7 +50,7 @@ func _check_solution() -> bool:
 	var i: int = 0
 	for interaction in _interaction_history:
 		var interaction_num: float = _interaction_history[interaction]
-		if interaction != _order[i]:
+		if interaction != _buttons[i]:
 			_interaction_history.clear()
 			print("[BUTTON_MODULE][CHECK SOLUTION] Button order incorrect, cleared")
 			return false
