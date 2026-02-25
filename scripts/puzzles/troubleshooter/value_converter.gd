a4777fc33f0f7987c141096ebd4ccb9d8f04ca5c
BuzyBeeC <brianc.gde@gmail.com>
2026-02-24 23:05:25 -0800

    Rewrite troubleshoot puzzle solution logic to randomize input but check output. Button has been converted to a discrete 'cycler' so it can switch between multiple values

--- scripts/puzzles/troubleshooter/value_converter.gd
+++ scripts/puzzles/troubleshooter/value_converter.gd
@@ -1,12 +1,15 @@
 class ValueConverter:
 	static func convert_discrete_to_intermediate(value: Variant, possible_values: Array) -> float:
-		var index: float = possible_values.find(value)
+		var index: float = float(possible_values.find(value))
 
 		if index < 0:
-			push_error("given value '%s' does not exist in possible_values" % value)
+			push_error("given value '%s' does not exist in possible_values: %s" % [
+				value, 
+				possible_values
+			])
 			return 0.0
 
-		return index
+		return index / (possible_values.size() - 1)
 
 
 	static func convert_float_to_intermediate(value: float, min_value: float, max_value: float) -> float:
@@ -14,7 +17,11 @@ class ValueConverter:
 			push_error("min_value must be strictly less than max_value")
 			return 0.0
 		if not (value <= max_value and value >= min_value):
-			push_error("given value '%f' was out of the interval [min_value, max_value]" % value)
+			push_error("given value '%f' was out of the interval [%f, %f]" % [
+				value,
+				min_value,
+				max_value
+			])
 			return 0.0
 
 		return (value - min_value) / (max_value - min_value)
@@ -24,7 +31,7 @@ class ValueConverter:
 		if possible_values.is_empty():
 			push_error("possible_values is empty")
 			return null
-			
+		
 		return possible_values[roundi(intermediate * (possible_values.size() - 1))]
 
 
@@ -34,7 +41,7 @@ class ValueConverter:
 			max_value: float,
 	) -> float:
 		if not min_value < max_value:
-			push_error("min_value must be strictly less than max_value")
+			push_error("min_value (%f) must be strictly less than max_value (%f)" % [min_value, max_value])
 			return 0.0
 		if not (intermediate <= 1.0 and intermediate >= 0.0):
 			push_error("given intermediate '%f' was out of the valid interval [0.0, 1.0]" % intermediate)
