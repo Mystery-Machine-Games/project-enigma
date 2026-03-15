aa4f5625edd71c606060c974a68626680e7d1d06
Lyle Watkins <85589327+llwatkin@users.noreply.github.com>
2026-03-15 01:45:28 -0700

    Added sfx to jigsaw

--- scripts/puzzles/jigsaw/puzzle_piece_organizer.gd
+++ scripts/puzzles/jigsaw/puzzle_piece_organizer.gd
@@ -43,10 +43,12 @@ func try_organize_pieces(node : JigsawPiece) -> void:
 						nodegroup.append_array(foundarr);
 						
 						pieces.append(nodegroup);
+						AudioManager.play_sound("click")
 						
 	if pieces.size() == 1:
 		print("emitted")
 		puzzleCompleteSignal.emit(get_children().front().get_hints());
+		AudioManager.play_sound("success")
 	#print("trying to organize")
 	#if b == true:
 		#return;
